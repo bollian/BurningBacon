e69ad5ce4ccc309acb8939b2ff45f75a22ebcd16
Calcifer <lordofthemaracas@gmail.com>
2015-01-28 22:25:44 -0500

    actualy add the #includes to bb.h

---
+++ src/bb.h
@@ -0,0 +1,9 @@
+#ifndef BURNINGBACON_BB_H
+#define BURNINGBACON_BB_H
+
+// listed in alphabetical order :)
+#include "bbmath.h"
+#include "bbsystem.h"
+#include "bbvector.h"
+
+#endif // BURNINGBACON_BB_H
